cab05925e5315f05deacc225996dc71f36e99fdf
stevem42 <elevated42@yahoo.com.au>
2022-09-17 08:38:11 +1000

    adjust mobile spacing on main

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,19 +8,19 @@ const Main = () => {
     <main id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] xs:px-8 w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <h1 className="py-2 text-gray-700">
+          <h1 className="mt-4 md:py-2 text-gray-700">
             <span className="text-[#5651e5]">Steve</span> Mckinnon
           </h1>
-          <h1 className="xs:mt-8 py-2 text-gray-700">
+          <h1 className="xs:mt-8 py-1 md:py-2 text-gray-700">
             Full-Stack <span className="text-[#5651e5]">Developer</span>
           </h1>
-          <p className="py-4 text-xl text-gray-600 max-w-[70%] m-auto">
+          <p className="py-2 md:py-4 text-xl text-gray-600 max-w-[90%] md:max-w-[70%] m-auto">
             <span className="font-bold">Committed to always learning</span> - I
             enjoy technology, and both the challenge and reward of programming.
             I am looking for an opportunity to grow my knowledge, and apply my
             skills.
           </p>
-          <div className="flex justify-center text-sm sm:text-lg ">
+          <div className="flex justify-center text-sm sm:text-md">
             <Link href="/#contact">
               <div className="flex justify-center items-center text-center">
                 <a className=" px-4 py-3 m-3 text-gray-100 mt-4 shadow-xl cursor-pointer shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-[#5651e5] to-[#709dff]">
@@ -42,7 +42,7 @@ const Main = () => {
               </div>
             </Link>
           </div>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+          <div className="flex items-center justify-between max-w-[330px] m-auto pt-2 md:py-4">
             <a
               target="_blank"
               rel="noreferrer"
